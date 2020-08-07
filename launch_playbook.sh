1ba15f98de9eca2de607f9b28e3ca847a36ca50b
dark2phoenix <dark3phoenix@gmail.com>
2020-08-07 10:42:55 -0400

    Fix ansible playbook

--- launch_playbook.sh
+++ launch_playbook.sh
@@ -3,4 +3,4 @@ set -o xtrace
 
 
 tdnf install ansible -y
-ansible playbook --connection=local --inventory 127.0.0.1 configure-photon.yml
+ansible-playbook --connection=local --inventory 127.0.0.1 configure-photon.yml
