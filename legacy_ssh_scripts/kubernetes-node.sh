76071d56a67d1fecb050141da2e58d1490d8e7b8
dark2phoenix <dark3phoenix@gmail.com>
2020-08-08 14:26:41 -0400

    Update node to pull from goodle

--- legacy_ssh_scripts/kubernetes-node.sh
+++ legacy_ssh_scripts/kubernetes-node.sh
@@ -62,7 +62,21 @@ systemctl restart docker
 #
 # Install kubernetes
 #
-tdnf install kubernetes kubernetes-kubeadm -y
+curl -o /etc/pki/rpm-gpg/GOOGLE-RPM-GPG-KEY https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+chmod 644 /etc/pki/rpm-gpg/GOOGLE-RPM-GPG-KEY
+rpm --import /etc/pki/rpm-gpg/GOOGLE-RPM-GPG-KEY
+
+cat > /etc/yum.repos.d/kubernetes.repo <<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/GOOGLE-RPM-GPG-KEY
+EOF
+
+tdnf install kubeadm kubectl kubelet -y
 systemctl enable --now kubelet
 
 #
