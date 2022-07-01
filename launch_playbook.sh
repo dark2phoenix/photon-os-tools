17dcb21fbfc9992bccaec1ebedf6ee33512d9693
dark2phoenix <dark3phoenix@gmail.com>
2022-07-01 12:05:13 -0400

    remove inventory call to 127.0.0.1

--- launch_playbook.sh
+++ launch_playbook.sh
@@ -5,4 +5,4 @@ set -o xtrace
 tdnf install ansible -y
 ansible-galaxy collection install community.general
 ansible-galaxy collection install ansible.posix
-ansible-playbook --connection=local --inventory 127.0.0.1 configure-photon.yml
+ansible-playbook --connection=local configure-photon.yml
